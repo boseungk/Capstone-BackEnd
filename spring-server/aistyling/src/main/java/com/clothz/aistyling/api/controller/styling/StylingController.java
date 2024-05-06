b0ca66895a8c266ef6cfd5d5c5cd82f3db0337d0
BoSeung <gda05189@gmail.com>
2024-05-06 16:39:24 +0900

    feat: StylingController getImageWithWords 추가
    - getImageWithWords로 이미지 생성을 요청하는 핸들러를 추가했습니다

--- spring-server/aistyling/src/main/java/com/clothz/aistyling/api/controller/styling/StylingController.java
+++ spring-server/aistyling/src/main/java/com/clothz/aistyling/api/controller/styling/StylingController.java
@@ -1,15 +1,17 @@
 package com.clothz.aistyling.api.controller.styling;
 
 import com.clothz.aistyling.api.ApiResponse;
+import com.clothz.aistyling.api.controller.styling.request.StylingWordsRequest;
 import com.clothz.aistyling.api.service.styling.StylingService;
 import com.clothz.aistyling.api.service.styling.response.StylingExampleResponse;
+import com.clothz.aistyling.api.service.styling.response.StylingImageResponse;
 import com.clothz.aistyling.global.jwt.userInfo.CustomUserDetails;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,12 +19,24 @@ import java.util.List;
 @RequestMapping("/api")
 @RestController
 public class StylingController {
+    private static final String AI_URL_WITH_WORDS = "/api/styling/words";
+    private static final String AI_URL_WITH_SENTENCES = "/api/styling/sentences";
     private final StylingService stylingService;
+
     @GetMapping("/styling")
     @PreAuthorize("hasRole('ROLE_USER')")
     public ApiResponse<List<StylingExampleResponse>> getUserInfo(
             @AuthenticationPrincipal final CustomUserDetails userDetails){
         final var imageAndPrompt = stylingService.getImageAndPrompt();
         return ApiResponse.ok(imageAndPrompt);
+    }
+
+    @PostMapping("/styling/words")
+    @PreAuthorize("hasRole('ROLE_USER')")
+    public ApiResponse<StylingImageResponse> getImageWithWords(
+            @RequestBody @Valid final StylingWordsRequest request,
+            @AuthenticationPrincipal final CustomUserDetails userDetails) throws JsonProcessingException {
+        final var imageWithWords = stylingService.getImageWithWords(AI_URL_WITH_WORDS, request, userDetails.getId());
+        return ApiResponse.ok(imageWithWords);
     }
 }
