45c67f9634c80aafde2006be9388a350e6e24b51
BoSeung <gda05189@gmail.com>
2024-05-06 16:42:12 +0900

    feat: StylingService getImageWithWords 기능 추가
    - ai api 서버에게 프롬프트와 유저 이미지를 통해 이미지 생성을 요청하는 기능을 추가했습니다
    - 현재 동기적으로 요청하지만 비동기적으로 리팩토링할 예정입니다

--- spring-server/aistyling/src/main/java/com/clothz/aistyling/api/service/styling/StylingService.java
+++ spring-server/aistyling/src/main/java/com/clothz/aistyling/api/service/styling/StylingService.java
@@ -1,11 +1,22 @@
 package com.clothz.aistyling.api.service.styling;
 
+import com.clothz.aistyling.api.controller.styling.request.PromptWithWordsRequest;
+import com.clothz.aistyling.api.controller.styling.request.StylingWordsRequest;
 import com.clothz.aistyling.api.service.styling.response.StylingExampleResponse;
+import com.clothz.aistyling.api.service.styling.response.StylingImageResponse;
 import com.clothz.aistyling.domain.styling.Styling;
 import com.clothz.aistyling.domain.styling.StylingRepository;
+import com.clothz.aistyling.domain.user.User;
+import com.clothz.aistyling.domain.user.UserRepository;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -14,13 +25,17 @@ import java.util.stream.Collectors;
 @Transactional
 @Service
 public class StylingService {
+    private final UserRepository userRepository;
     private final StylingRepository stylingRepository;
+    private final ObjectMapper objectMapper;
+    private final WebClient webClient;
+
     public List<StylingExampleResponse> getImageAndPrompt() {
         // 임시 Mock Example 객체 저장, 이후 삭제 예정
         final var mockExamples = createMockExample();
         stylingRepository.saveAll(mockExamples);
 
-        List<Styling> examples = stylingRepository.findAll();
+        final List<Styling> examples = stylingRepository.findAll();
         return examples.stream()
                 .map(StylingExampleResponse::from)
                 .collect(Collectors.toList());
@@ -37,4 +52,29 @@ public class StylingService {
                 .build();
         return List.of(example1, example2);
     }
+
+    public StylingImageResponse getImageWithWords(final String requestUrl, final StylingWordsRequest request, final Long id) throws JsonProcessingException {
+        final User user = userRepository.findById(id).orElseThrow(
+                () -> new UsernameNotFoundException("User not found")
+        );
+        final List<String> imageUrls = deserializeImageUrls(user.getUserImages());
+        final var jsonResponse = post(requestUrl, PromptWithWordsRequest.of(request.words(), imageUrls), StylingImageResponse.class);
+        return jsonResponse.block();
+    }
+
+    private List<String> deserializeImageUrls(final String imgUrls) throws JsonProcessingException {
+        if (null == imgUrls)
+            return List.of();
+        return objectMapper.readValue(imgUrls, new TypeReference<List<String>>() {
+        });
+    }
+
+    private <T> Mono<T> post(final String url, final Object request, final Class<T> responseType) {
+        return webClient.post()
+                .uri(url)
+                .bodyValue(request)
+                .retrieve()
+                .bodyToMono(responseType);
+    }
+
 }
