519c18382f3d76dcf931850981b5088fd1683e0e
BoSeung <gda05189@gmail.com>
2024-05-08 14:57:41 +0900

    feat: Customized Error 타입 정의
    - Customized Error 타입을 정의했습니다.

---
+++ src/main/java/com/clothz/aistyling/global/error/Exception401.java
@@ -0,0 +1,16 @@
+package com.clothz.aistyling.global.error;
+
+import com.clothz.aistyling.global.api.ApiResponse;
+import org.springframework.http.HttpStatus;
+
+public class Exception401 extends RuntimeException{
+    public Exception401(final String message) {
+        super(message);
+    }
+    public ApiResponse<Exception401> body() {
+        return ApiResponse.error(HttpStatus.UNAUTHORIZED, getMessage());
+    }
+    public HttpStatus status() {
+        return HttpStatus.UNAUTHORIZED;
+    }
+}
