93b2f994fbfafe6e70971bf8e393ceda1dd1680e
nurim <dhkdwk70@gmail.com>
2024-05-10 18:01:24 +0900

    feat: Controller에 @Tag, @Operation, @Parameter추가
    Swagger 상세 설정을 위해서 추가했습니다.

--- src/main/java/com/clothz/aistyling/api/controller/home/HomeController.java
+++ src/main/java/com/clothz/aistyling/api/controller/home/HomeController.java
@@ -3,6 +3,8 @@ package com.clothz.aistyling.api.controller.home;
 import com.clothz.aistyling.api.ApiResponse;
 import com.clothz.aistyling.api.service.home.HomeService;
 import com.clothz.aistyling.api.service.home.response.HomeResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,11 +15,13 @@ import java.util.List;
 @RequiredArgsConstructor
 @RequestMapping("/api")
 @RestController
+@Tag(name = "HomeController", description = "소개 및 메인 화면")
 public class HomeController {
 
     private final HomeService homeService;
 
     @GetMapping("/home")
+    @Operation(summary = "소개 및 메인 화면", description = "서비스를 표현할 수 있는 이미지 및 글 작성")
     public ApiResponse<List<HomeResponse>> getHomeInfo() {
         List<HomeResponse> imageAndSentence = homeService.getImageAndSentence();
         return ApiResponse.ok(imageAndSentence);
--- src/main/java/com/clothz/aistyling/api/controller/user/UserController.java
+++ src/main/java/com/clothz/aistyling/api/controller/user/UserController.java
@@ -6,11 +6,15 @@ import com.clothz.aistyling.api.controller.user.request.UserUpdateRequest;
 import com.clothz.aistyling.api.service.user.UserService;
 import com.clothz.aistyling.api.service.user.response.UserImagesResponse;
 import com.clothz.aistyling.api.service.user.response.UserInfoResponse;
-import com.clothz.aistyling.api.service.user.response.UserSingUpResponse;
+import com.clothz.aistyling.api.service.user.response.UserSignUpResponse;
 import com.clothz.aistyling.api.service.user.response.UserUpdateResponse;
 import com.clothz.aistyling.global.jwt.userInfo.CustomUserDetails;
 import com.clothz.aistyling.global.s3.S3Service;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -27,12 +31,20 @@ import java.util.List;
 @RequestMapping("/api")
 @RestController
 @Slf4j
+@Tag(name = "UserController", description = "회원정보 조회하기, 수정하기, 사진변경, 회원가입")
 public class UserController {
     private final UserService userService;
     private final S3Service s3Service;
 
     @PostMapping("/signup")
-    public ApiResponse<UserSingUpResponse> signUp(
+    @Operation(summary = "회원가입", description = "사용자 이메일, 닉네임, 비밀번호를 입력받아 회원가입한다")
+    @Parameters({
+            @Parameter(name = "request.email", description = "이메일 형식으로 작성해주세요"),
+            @Parameter(name = "request.nickname", description = "3에서 20자 이내여야 합니다"),
+            @Parameter(name = "request.password", description = "8에서 20자 이내여야 합니다. " +
+                    "영문, 숫자, 특수문자가 포함되어야하고 공백이 포함될 수 없습니다.")
+    })
+    public ApiResponse<UserSignUpResponse> signUp(
             @RequestBody @Valid
             final UserCreateRequest request){
         final var userSingUpResponse = userService.signUp(request);
@@ -41,6 +53,7 @@ public class UserController {
 
     @GetMapping("/users")
     @PreAuthorize("hasRole('ROLE_USER')")
+    @Operation(summary = "회원정보 조회하기", description = "토큰을 기반으로 사용자의 회원정보를 조회한다")
     public ApiResponse<UserInfoResponse> getUserInfo(
             @AuthenticationPrincipal final CustomUserDetails userDetails
     ) throws JsonProcessingException {
@@ -50,6 +63,8 @@ public class UserController {
 
     @PostMapping(value = "/users/images", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     @PreAuthorize("hasRole('ROLE_USER')")
+    @Operation(summary = "회원정보 사진 변경", description = "토큰을 기반으로 사용자의 이미지를 변경한다")
+    @Parameter(name = "images", description = "이미지들을 업로드 합니다")
     public ApiResponse<UserImagesResponse> uploadUserImg(
             final List<MultipartFile> images,
             @AuthenticationPrincipal final CustomUserDetails userDetails
@@ -61,6 +76,12 @@ public class UserController {
 
     @PutMapping("/users")
     @PreAuthorize("hasRole('ROLE_USER')")
+    @Operation(summary = "회원정보 수정하기", description = "토큰을 기반으로 사용자의 회원정보를 수정한다")
+    @Parameters({
+            @Parameter(name = "request.nickname", description = "3에서 20자 이내여야 합니다."),
+            @Parameter(name = "request.password", description = "8에서 20자 이내여야 합니다. " +
+                    "영문, 숫자, 특수문자가 포함되어야하고 공백이 포함될 수 없습니다.")
+    })
     public ApiResponse<UserUpdateResponse> updateUser(@RequestBody final UserUpdateRequest request, @AuthenticationPrincipal final CustomUserDetails userDetails) throws JsonProcessingException{
         final UserUpdateResponse response = userService.updateUser(request, userDetails.getId());
         return ApiResponse.ok(response);
