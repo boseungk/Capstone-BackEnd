cf861e1719d0cb5a53dafdb2525ba0d92ae413e1
tgg <qazplmtfgcv123@gmail.com>
2024-05-10 20:18:22 +0900

    update Exception400,Exception401,Exception403,Exception404,Exception500

--- src/main/java/com/clothz/aistyling/global/error/Exception401.java
+++ src/main/java/com/clothz/aistyling/global/error/Exception401.java
@@ -4,8 +4,8 @@ import com.clothz.aistyling.global.api.ApiResponse;
 import org.springframework.http.HttpStatus;
 
 public class Exception401 extends RuntimeException{
-    public Exception401(final String message) {
-        super(message);
+    public Exception401(final ErrorCode code) {
+        super(code.getMessage());
     }
     public ApiResponse<Exception401> body() {
         return ApiResponse.error(HttpStatus.UNAUTHORIZED, getMessage());
--- src/main/java/com/clothz/aistyling/global/error/Exception500.java
+++ src/main/java/com/clothz/aistyling/global/error/Exception500.java
@@ -4,8 +4,8 @@ import com.clothz.aistyling.global.api.ApiResponse;
 import org.springframework.http.HttpStatus;
 
 public class Exception500 extends RuntimeException{
-    public Exception500(final String message) {
-        super(message);
+    public Exception500(final ErrorCode code) {
+        super(code.getMessage());
     }
     public ApiResponse<Exception500> body() {
         return ApiResponse.error(HttpStatus.INTERNAL_SERVER_ERROR, getMessage());
