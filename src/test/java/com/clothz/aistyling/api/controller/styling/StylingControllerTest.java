18682d500c0761b70fa54041b4e022a6b88bfb84
BoSeung <gda05189@gmail.com>
2024-06-23 15:38:43 +0900

    test: StylingController getImageWithWords 테스트 코드 추가
    - mockMvc 테스트를 추가했습니다

--- src/test/java/com/clothz/aistyling/api/controller/styling/StylingControllerTest.java
+++ src/test/java/com/clothz/aistyling/api/controller/styling/StylingControllerTest.java
@@ -1,27 +1,39 @@
 package com.clothz.aistyling.api.controller.styling;
 
+import com.clothz.aistyling.api.controller.styling.request.StylingWordsRequest;
 import com.clothz.aistyling.api.service.styling.StylingService;
 import com.clothz.aistyling.api.service.styling.response.StylingExampleResponse;
 import com.clothz.aistyling.domain.styling.Styling;
+import com.clothz.aistyling.util.WithMockCustomUser;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.EntityManager;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(controllers = StylingController.class)
+@AutoConfigureMockMvc
+@MockBean(JpaMetamodelMappingContext.class)
+@SpringBootTest
 class StylingControllerTest {
 
     @Autowired
@@ -30,11 +42,12 @@ class StylingControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+
     @MockBean
     private StylingService stylingService;
 
-    @WithMockUser
     @DisplayName("예시 이미지와 프롬프트를 가져온다.")
+    @WithMockUser
     @Test
     void getUserInfo() throws Exception {
         //given
@@ -58,5 +71,27 @@ class StylingControllerTest {
                 .andExpect(jsonPath("$.data[1].image").value("images2"))
                 .andExpect(jsonPath("$.data[1].prompt").value("prompt example 2"));
 
+    }
+
+    @DisplayName("프롬프트를 입력하면 이미지를 생성한다.")
+    @WithMockCustomUser
+    @Test
+    void getImageWithWords() throws Exception {
+        //given
+        final StylingWordsRequest request = new StylingWordsRequest("스트릿");
+        final var response = CompletableFuture.completedFuture("generatedImage.png");
+
+        given(stylingService.getImageWithWords(request, 1L))
+                .willReturn(response);
+
+        //when
+        //then
+        mockMvc.perform(
+                        post("/api/styling/words")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 }
